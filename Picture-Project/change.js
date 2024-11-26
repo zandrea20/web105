a7134d570273d6acfbdd067c72d0d9507dc29b7f
zandrea20 <zandrea@students.mchenry.edu>
2024-11-26 01:17:46 -0600

    Add files via upload

---
+++ Picture-Project/change.js
@@ -0,0 +1,20 @@
+function change01() {
+    
+    document.getElementById("large").src ="img/hyacinth-macaw-7501470_640.jpg";
+}
+
+function change02() {
+
+    document.getElementById("large").src = "img/hyacinth-macaw'-7501470_640.jpg";
+}
+
+function change03() {
+
+    document.getElementById("large").src = "img/macaw-4448598_640.jpg";
+}
+
+function change04() {
+
+    document.getElementById("large").src = "img/macaw'-4448598_640.jpg";
+}
+
